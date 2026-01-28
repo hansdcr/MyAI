c8b7003e41b44b2d25fec2fbf1a2543dd9a2ee42
464501551@qq.com <464501551@qq.com>
2026-01-28 17:51:45 +0800

    添加项目日志系统

--- app/main.py
+++ app/main.py
@@ -4,13 +4,20 @@
 # @Author   : Gelin
 # @File     : main.py
 
+import logging
 from fastapi import FastAPI
 from core.config import get_settings
+from app.infrastructure.logging import setup_logging
 
-
+# 1、加载配置信息
 settings = get_settings()
-print(settings)
 
+# 2、初始化日志系统
+setup_logging()
+logger = logging.getLogger()
+
+
+logger.info("测试..")
 
 app = FastAPI()
 
