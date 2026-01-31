4d77084674d37f5a93e9f44d755623d714d92716
464501551@qq.com <464501551@qq.com>
2026-01-31 20:15:39 +0800

    添加OpenAI Agent调用方法 并添加测试用例

--- app/domain/models/app_config.py
+++ app/domain/models/app_config.py
@@ -8,20 +8,21 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+from pydantic import BaseModel, ConfigDict, HttpUrl, Field
 
-from pydantic import BaseModel, ConfigDict, HttpUrl,Field
 
 class LLMConfig(BaseModel):
     """ 语言模型配置 """
-    base_url: HttpUrl = "https://api.deepseek.com" # 基础URL地址
-    api_key: str = "" # API密钥
-    model_name:str = "deepseek-reasoner" # 推理模型如果传递了tools底层会自动切换deepseek-chat
-    temperature: float = Field(default=0.7) # 温度
-    max_tokens: int = Field(8192,ge=0) # 最大输出token数, 值要大于等于0
+    base_url: HttpUrl = "https://api.deepseek.com"  # 基础URL地址
+    api_key: str = ""  # API密钥
+    model_name: str = "deepseek-reasoner"  # 推理模型如果传递了tools底层会自动切换deepseek-chat
+    temperature: float = Field(default=0.7)  # 温度
+    max_tokens: int = Field(8192, ge=0)  # 最大输出token数, 值要大于等于0
+
 
 class AppConfig(BaseModel):
     """ 应用配置信息，包含Agent配置、LLM提供商、A2A网络、MCP服务配置等"""
-    llm_config: LLMConfig # 语言模型配置
+    llm_config: LLMConfig  # 语言模型配置
 
     # Pydantic配置，允许传递额外字段初始化
     model_config = ConfigDict(extra="allow")
