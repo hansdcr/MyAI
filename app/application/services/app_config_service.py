7d1b6afcd837d8a6759018d1623df28403ed204f
464501551@qq.com <464501551@qq.com>
2026-01-31 14:08:08 +0800

    生成和解析App配置文件

---
+++ app/application/services/app_config_service.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date     : 2026/01/31
+# @Author   : Gelin
+# @File     : app_config_service.py
+
+import logging
+from app.domain.models.app_config import AppConfig, LLMConfig
+from app.domain.repositories.app_config_repository import AppConfigRepository
+
+logger = logging.getLogger(__name__)
+
+
+class AppConfigService:
+    """ 应用配置服务 """
+    def __init__(self,app_config_respository: AppConfigRepository) -> None:
+        """构造函数，完成应用配置服务的初始化 """
+        self.app_config_respository = app_config_respository
+    
+    def _load_app_config(self) -> AppConfig:
+        """加载获取所有的应用配置"""
+        return self.app_config_respository.load()
+
+    def get_llm_config(self) -> LLMConfig:
+        """加载获取LLM提供商配置"""
+        return self._load_app_config().llm_config
+    
+    def update_llm_config(self,llm_config:LLMConfig) -> LLMConfig:
+        """根据传递的llm_config更新语言模型提供商配置"""
+        # 1、获取应用配置
+        app_config = self._load_app_config()
+        # 2、判断api_key是否为空
+        if not llm_config.api_key.strip():
+            llm_config.api_key = app_config.llm_config.api_key
+        # 3、调用函数更新app_config
+        app_config.llm_config = llm_config
+        self.app_config_respository.save(app_config)
+    
+        return app_config.llm_config
