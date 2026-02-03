83365468cc15fcd56c366e498a7cd59973987cab
464501551@qq.com <464501551@qq.com>
2026-02-03 15:19:45 +0800

    添加Task任务协议

---
+++ app/domain/external/message_queue.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time         :   2026/2/3
+# @Author       :   Gelin
+# @Software     :   PyCharm
+# @File         :   message_queue.py.py
+
+from typing import Protocol
+
+class MessageQueue(Protocol):
+    """消息队列协议"""
+    pass
